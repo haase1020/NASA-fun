8776dd5c13758357b42354b58d6019502cf2f828
Mandi <haase1020@gmail.com>
2020-11-30 15:08:15 -0500

    update javascript

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const resultsNav = document.getElementById('resultsNav')
+const favoritesNav = document.getElementById('favoritesNav')
+const imagesContainer = document.querySelector('.images-container');
+const saveConfirmed = document.querySelector('.save-confirmed');
+const loader = document.querySelector('.loader');
+
 
 
 // NASA api
